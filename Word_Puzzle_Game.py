24bbc178e49f4944bdfc8bc08265a4091577772f
cdpatra <109278272+cdpatra@users.noreply.github.com>
2022-09-15 01:43:05 +0530

    Update Word_Puzzle_Game.py

--- Word_Puzzle_Game.py
+++ Word_Puzzle_Game.py
@@ -10,19 +10,24 @@ print()
 print()
 words = [('ragend', 'garden'), ('willpo', 'pillow'), ('pomcetru', 'computer'), ('hawle', 'whale'),
          ('iudsq', 'squid'), ('hskra', 'shark'), ('eociv', 'voice'), ('anrel', 'learn'), ('oocdrt', 'doctor'), ('eonrufldw', 'wonderful'), ('nsmgmiiw', 'swimming'), ('cpelin', 'pencil')]
-score = 0
-random_index_list = []
-while len(random_index_list) != 10:
-    rand_var = random.randint(0, 9)
-    if random_index_list.count(rand_var) == 0:
-        random_index_list.append(rand_var)
-for e in range(5):
-    print("Arrange the letters to form a valid word:")
-    print((words[random_index_list[e]][0]).upper())
-    if words[random_index_list[e]][1] == (input()).lower():
-        print('Correct\n')
-        score += 1
+while True:
+    score = 0
+    random_index_list = []
+    while len(random_index_list) != 10:
+        rand_var = random.randint(0, 9)
+        if random_index_list.count(rand_var*7 % 10) == 0:
+            random_index_list.append(rand_var*7 % 10)
+    for e in range(5):
+        print("Arrange the letters to form a valid word:")
+        print((words[random_index_list[e]][0]).upper())
+        if words[random_index_list[e]][1] == (input()).lower():
+            print('Correct\n')
+            score += 1
+        else:
+            print('Wrong\n')
+            score -= 1
+    print("Net score is :", score)
+    if input("Want to play again [y/n]: ") == 'y':
+        pass
     else:
-        print('Wrong\n')
-        score -= 1
-print("Net score is :", score)
+        break
